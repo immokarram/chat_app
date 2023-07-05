ceb6a4fbd59b4b215f7be8167c16151640d666ee
Mokarram Ali <88176270+immokarram@users.noreply.github.com>
2023-07-05 14:51:39 +0530

    Add files via upload

---
+++ src/reducers/user.js
@@ -0,0 +1,12 @@
+import myImage from '../images/mok.jpg';
+
+export default function user(state = {}) {
+ 
+   
+    return {
+      id: 1,
+      name: "Mokarram Ali ",
+      image : myImage
+    }
+  
+  }
